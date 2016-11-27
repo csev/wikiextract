784cf520c06266d2c1aab27cb067643387e6367c
Charles Severance <csev@umich.edu>
2016-11-26 22:12:51 -0500

    More HTML :)

--- index.php
+++ index.php
@@ -1,11 +1,33 @@
 <?php
+
+function show_directory() {
+    header('Content-Type: text/html');
+    echo("<h1>Welcome to the Wiki Archive</h1>\n");
+    echo("<ul>\n");
+    foreach (glob("html/*/Main") as $filename) {
+        $pieces = explode('/',$filename);
+        if ( count($pieces) != 3 ) continue;
+        echo('<li><a href="/wiki/index.php/'.$pieces[1].':'.$pieces[2].'">');
+        echo($pieces[1].':'.$pieces[2]."</a></li>\n");
+    }
+}
+
+if ( !isset($_SERVER['PATH_INFO']) ) {
+    show_directory();
+    return;
+}
+
 $path = $_SERVER['PATH_INFO'];
 if ( strpos($path,'/') !== 0 ) {
-    die('Bad format');
+    show_directory();
+    return;
 }
 $path = substr($path,1);
 $pieces = explode(':',$path);
-if ( count($pieces) != 2 ) die('Bad format:');
+if ( count($pieces) != 2 ) {
+    show_directory();
+    return;
+}
 /*
 echo("<pre>\n");
 var_dump($pieces);
